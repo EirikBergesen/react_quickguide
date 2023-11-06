2b5c05cf36133e55e98648b406f88e1400f6e298
EirikBergesen <eirikbr@yahoo.com>
2023-11-06 16:30:11 +0100

    Made progress on tut

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+// https://react.dev/learn/tutorial-tic-tac-toe
+
+// usestate is a special fuction that you can call from a component
+// to allow it to 'remember things'
+import { useState } from "react";
 
 // Default keyword tells other files this is 
 // the main function of this file
@@ -21,24 +26,60 @@ export default function Square() {
 }
 */
 
+function Board( { isXNext, squares, onPlay }) {
+  // State is a way to store data in a component
+  // private to that component
+  // const [squares, setSquares] = useState(Array(9).fill(null));
+  // const [isXNext, setIsXNext] = useState(true);
+  // const winner = calculateVinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner is: " + winner;
+  } else {
+    status = "Next player: " + (isXNext ? "X" : "O");
+  }
 
-export default function Board() {
+  function handleClick(squareIndex) {
+    if (squares[squareIndex] || calculateVinner(squares)) {
+      return;
+    }
+
+
+    const nextSquares = [...squares];
+    // nextSquares = squares.slice();
+    if (isXNext) {
+      nextSquares[squareIndex] = 'X';
+    }
+    else {
+      nextSquares[squareIndex] = 'O';
+    }
+    onPlay(nextSquares)
+    // setIsXNext(!isXNext);
+    // setSquares(newSquares);
+    // setSquare will re-render component, as well as child components, board, and squares
+  }
+
   return (
     <>
+    <div
+      className="status"
+      >
+        {status}
+        </div>
     <div className="board-row">
-      <Square value="2"/>
-      <Square />
-      <Square />
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
     </div>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
     </div>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
     </div>
     </>
     /*
@@ -47,13 +88,25 @@ export default function Board() {
   );
 }
 
-function Square({ value }) { // { value } indicates this component can be passed a prop called value
+// function Square({ value }) { // { value } indicates this component can be passed a prop called value
   // Functions need to start with capital letters.
+
+function Square({ value, onSquareClick }) {
+  return <button
+    className="square"
+    onClick={onSquareClick}
+    >
+      {value}
+  </button>
+  /*
+  // null here is a initial value
+  const [value, setValue] = useState(null);
+  
 
   function handleClick() {
     console.log('Clicked!');
+    setValue('X');
   }
-
   return (
     <button
       className="square"
@@ -62,4 +115,55 @@ function Square({ value }) { // { value } indicates this component can be passed
       {value}
     </button>
   );
+  */
+}
+// function calculateVinner(squares) {
+const calculateVinner = (squares) => {
+  const winningLines = [
+    [0, 1, 2], // top row
+    [3, 4, 5], // middle row
+    [6, 7, 8], // bottom row
+    [0, 3, 6], // column 1
+    [1, 4, 7], // column 2
+    [2, 5, 8], // column 3
+    [0, 4, 8], // diagonal 1
+    [2, 4, 6] // diagonal 2
+  ];
+
+  for (let i = 0; i < winningLines.length; i++) {
+    const [a, b, c] = winningLines[i];
+    const sign = squares[a]
+    if (squares[b] === sign && squares[c] === sign) {
+      return sign
+    }
+  }
+  return null;
+}
+
+
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+
+  function handlePlay(nextSquares) {
+
+  }
+  return (
+    <div
+      className="game"
+      >
+        <div
+        className="game-board">
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        </div>
+        <div
+          className="game-info"
+          >
+
+          </div>
+      </div>
+  )
 }
